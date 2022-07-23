fe3b1ee9ffca61d6be4b41abe0c255446f73b456
MRyutaro <m2002r1028@icloud.com>
2022-07-23 15:11:47 +0900

    easier to read

--- Initial_setting/I2Ctest.py
+++ Initial_setting/I2Ctest.py
@@ -7,5 +7,12 @@ line = ser.readline()
 while True:
     line = ser.readline()
     line_disp = line.strip().decode('UTF-8')
-    print(line_disp)
+    line_disp = line_disp.split(',')
+    if len(line_disp) == 7:
+        accel = "【加速度】x:" + line_disp[0] + " y:" + \
+            line_disp[1] + " z:" + line_disp[2]
+        gyro = "【角速度】x:" + line_disp[3] + " y:" + \
+            line_disp[4] + " z:" + line_disp[5]
+        light = "【ライト】" + line_disp[6]
+        print(accel + gyro + light)
     time.sleep(0.2)
